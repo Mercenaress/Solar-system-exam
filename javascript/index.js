61bfc0dfa87e916e08be5fecf15b0525eb52c5b3
Mercenaress <linden9707@gmail.com>
2023-02-21 14:19:25 +0100

    Added basic search functionality

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,45 @@
 const planetURL = "https://majazocom.github.io/Data/solaris.json";
 let planetCard = document.querySelector(".planet-wrapper");
+let planetArray = [];
+
+fetchPlanetaryList();
 
 function fetchPlanetaryList() {
     fetch(planetURL)
     .then(planetList => planetList.json())
     .then((planetList) => {
-         for(let i = 0; i < planetList.length; i++) {
-
+        planetArray = planetList
+        for(let i = 0; i < planetList.length; i++) {
+            
             let spaceOrb = document.createElement("article")
             spaceOrb.className = `${planetList[i].name}`;
             spaceOrb.innerHTML = (`
             <h2>${planetList[i].name}</h2>
             `)
-
+            
+            
             planetCard.appendChild(spaceOrb);
+            
+            function infoPageRedirect() {
+                spaceOrb.addEventListener('click', () => {
+                    console.log(`${planetList[i].name}`);
+                    // window.location.href = 'result.html';
+                })
+            }
+            infoPageRedirect();
+
 
              console.log(planetList[i]);
          } 
         });
     }
-    
-    fetchPlanetaryList();
+
+document.querySelector('.search-button').addEventListener('click', () => {
+    let input = document.querySelector('.search-input').value 
+    console.log(input);
+
+    let index = planetArray.findIndex(planet => planet.name.toLowerCase() === input.toLowerCase())
+
+    console.log(index);
+})
     
