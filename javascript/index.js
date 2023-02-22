2120efb0c68ad9f94f460044a0ebc48062690f39
Mercenaress <linden9707@gmail.com>
2023-02-22 16:36:27 +0100

    Finally got redirect to work

--- javascript/index.js
+++ javascript/index.js
@@ -1,54 +1,80 @@
 const planetURL = "https://majazocom.github.io/Data/solaris.json";
-let planetContainer = document.querySelector(".planet-wrapper");
-let planetInformationCard = document.querySelector(".planetary-info")
+const planetsContainer = document.querySelector(".planet-wrapper");
+const planetInfoContainer = document.querySelector(".planetary-info");
 let planetArray = [];
+let pickedPlanet = [];
 
-getLocalStorage();
 fetchPlanetaryList();
+getLocalStorage();
 
 function getLocalStorage() {
     let currentPlanet = JSON.parse(localStorage.getItem('planetInfo'));
+    let planetInfoCard = document.createElement("article");
+    planetInfoCard.className = 'planet-card';
+    planetInfoCard.innerHTML = (`
+    <h2>${currentPlanet.name}</h2>
+    <h3>${currentPlanet.latinName}</h3>
+    <p>${currentPlanet.desc}</p>
+    <section>
+    <h5>OMKRETS</h5>
+    <p>${currentPlanet.circumference}km</p>
+    <h5>KM FRÅN SOLEN</h5>
+    <p>${currentPlanet.distance}km</p>
+    <h5>MAX TEMPERATUR</h5>
+    <p>${currentPlanet.temp.day}°C</p>
+    <h5>MIN TEMPERATUR</h5>
+    <p>${currentPlanet.temp.night}°C</p>
+    </section>
+    <section>
+    <h5>MÅNAR</h5>
+    <p>${currentPlanet.moons}</p>
+    </section>
+    `);
+
+    if (planetInfoContainer) {
+        planetInfoContainer.appendChild(planetInfoCard);
+    }
 }
 
 // Fetches the planet list from API and turns them into clickable articles 
 function fetchPlanetaryList() {
     fetch(planetURL)
     .then(planetList => planetList.json())
-    .then((planetList) => {
+    .then((planetList) => { 
         planetArray = planetList
         for(let i = 0; i < planetList.length; i++) {
-            
-            let spaceOrb = document.createElement("article")
+
+            let spaceOrb = document.createElement("article");
+            let planetInfoCard = document.createElement("article");
             spaceOrb.className = `${planetList[i].name}`;
             spaceOrb.innerHTML = (`
             <h2>${planetList[i].name}</h2>
             `)
+
+            planetInfoCard.className = "planet-card";
+            planetInfoCard.innerHTML = (`<p>${planetList[i].name}</p>`)
             
-            planetContainer.appendChild(spaceOrb);
+           if (planetsContainer) {
+               planetsContainer.appendChild(spaceOrb);
+           } 
 
-            // Makes the planets clickable and redirects to the next page
-            function infoPageRedirect() {
-                spaceOrb.addEventListener('click', () => {
-                    console.log(`${planetList[i].name}`);
-                    localStorage.setItem('planetInfo', JSON.stringify(planetList[i]));
-                    window.location.href = 'result.html';
-                })
-            }
-            infoPageRedirect();
+           spaceOrb.addEventListener('click', () => {
+               localStorage.setItem('planetInfo', JSON.stringify(planetList[i]));
+               window.location.href = 'result.html';
+           })
+            
+           console.log(planetList[i]);
+        } 
+    });
+}
 
-
-             console.log(planetList[i]);
-         } 
-        });
-    }
-
-document.querySelector('.search-button').addEventListener('click', () => {
-    let input = document.querySelector('.search-input').value 
-    console.log(input);
-
-    let index = planetArray.findIndex(planet => planet.name.toLowerCase() === input.toLowerCase())
-    localStorage.setItem('planetInfo', JSON.stringify(planetArray[index]));
-    console.log(index);
-})
-
-  
+function searchFunction() {
+    document.querySelector('.search-button').addEventListener('click', () => {
+        let input = document.querySelector('.search-input').value 
+        console.log(input);
+        
+        let index = planetArray.findIndex(planet => planet.name.toLowerCase() === input.toLowerCase())
+        localStorage.setItem('planetInfo', JSON.stringify(planetArray[index]));
+        console.log(index);
+    })
+}
